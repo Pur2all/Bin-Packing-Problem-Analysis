f6e388fc195235f1b371ca99d5c311a95039b186
Pur2all <39830676+Pur2all@users.noreply.github.com>
2020-08-07 18:42:17 +0200

    Removed useless code and refactored other things

--- Source Code/Bin-Packing-Problem-Analysis.py
+++ Source Code/Bin-Packing-Problem-Analysis.py
@@ -56,7 +56,7 @@ def bin_pack_merging(objects, bins):
 
 def calculate_size_of_bins(objects, number_of_bins):
     sum_of_objects_size = sum(objects)
-        
+    
     if sum_of_objects_size % number_of_bins != 0:
         rest = sum_of_objects_size % number_of_bins
         i = np.random.randint(0, number_of_objects)
@@ -68,34 +68,20 @@ def calculate_size_of_bins(objects, number_of_bins):
     return size_of_bins
 
 
-def get_i_star(number_of_objects, objects, number_of_bins):
-    i_star = -1
-
-    for i in range(number_of_bins - 1):
-        remanings_sum = sum(objects[i + 1:])
-        quantity = remanings_sum / (number_of_bins - (i + 1))
-
-        if objects[i] > quantity:
-            i_star = i
-        else:
-            break
-    
-    return i_star + 1
-
-
 def display_chart(object_history, bins_history, results):
     fig, ax = plt.subplots(2, 1, figsize=(16, 9), subplot_kw={"projection": "3d"})
+    fig.canvas.set_window_title("Bin Packing Problem Analysis")
     ax[0].axis("off")
     row_labels = [f"Bins: {n_bins}, Objects: {n_objects}" for n_bins, n_objects in zip(bins_history, objects_history)]
-    ax[0].table(cellText=[(first, second, lower_bound) for first, second, lower_bound in results], 
+
+    ax[0].table(cellText=[(lpt, merging) for lpt, merging in results], 
                 rowLabels=row_labels, 
-                colLabels=("First Algorithm", "Second Algorithm", "Lower Bound"),
+                colLabels=("LPT Algorithm", "Merging Algorithm"),
                 cellLoc="center", 
                 loc="center",
                 colWidths=[.2] * 3)
-    ax[1].plot(objects_history, bins_history, list(map(lambda element: element[0], results)), marker="o", color="red", label="first algorithm")
-    ax[1].plot(objects_history, bins_history, list(map(lambda element: element[1], results)), marker="o", color="blue", label="second algorithm")
-    ax[1].plot(objects_history, bins_history, list(map(lambda element: element[2], results)), marker="o", color="green", label="lower bound")
+    ax[1].plot(objects_history, bins_history, list(map(lambda element: element[0], results)), linestyle="None", marker="o", color="red", label="first algorithm")
+    ax[1].plot(objects_history, bins_history, list(map(lambda element: element[1], results)), linestyle="None", marker="o", color="blue", label="second algorithm")
     
     ax[1].set_xlabel("Number of objects to pack")
     ax[1].set_ylabel("Number of bins")
@@ -114,7 +100,7 @@ if __name__ == "__main__":
     number_of_experiments = int(input("How many experiments do you want to do? "))
 
     for experiment in range(number_of_experiments):
-        print(f"\nExperiments number {experiment + 1}")
+        print(f"\nExperiment number {experiment + 1}")
 
         number_of_bins = int(input("Insert number of bins: "))
         number_of_objects = int(input("Insert number of objects to pack: "))
@@ -128,23 +114,9 @@ if __name__ == "__main__":
         merging_algorithm_result = bin_pack_merging(objects, bins)
 
         temp_objects = sorted(objects)[::-1]
-        
-        if temp_objects[0] < size_of_bins:
-            r_vector = [size_of_bins] * number_of_bins
-        else:
-            i_star = get_i_star(number_of_objects, temp_objects, number_of_bins)
-
-            r_vector = temp_objects[:i_star]
-
-            if i_star < number_of_bins: 
-                remainings_sum = sum(temp_objects[i_star:])
-                value = remainings_sum / (number_of_bins - i_star)
-                r_vector += [value] * (number_of_bins - i_star)
-
-        r_vector_result = sum(abs(size_of_bins - value) for value in r_vector)
 
         objects_history += [number_of_objects]
         bins_history += [number_of_bins]
-        results += [(lpt_algorithm_result, merging_algorithm_result, r_vector_result)]
+        results += [(lpt_algorithm_result, merging_algorithm_result)]
 
     display_chart(objects_history, bins_history, results)
